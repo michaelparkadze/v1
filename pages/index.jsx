9a94aa59b0620f0f2d4495f51c2515c3d72d0c59
Michael Parkadze <52443367+michaelparkadze@users.noreply.github.com>
2021-04-23 19:42:44 +0300

    [Update] Initial home welcome section

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,18 +1,29 @@
 import Head from "next/head";
-import Button from "../components/Button";
 import Layout from "../components/Layout";
+import Button from "../components/Button";
+import styles from "../styles/home.module.scss";
 
 export default function Home() {
   return (
-    <Layout>
+    <>
       <Head>
         <title>Michael Parkadze</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div>
-        <h2>Hello World!</h2>
-        <Button />
-      </div>
-    </Layout>
+      <Layout>
+        <div className={styles.container}>
+          <section>
+            <h1>
+              Hello I am Michael. <br /> A software engineer.
+            </h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit dapibus
+              porta lorem morbi hendrerit. Maecenas et, at quis purus.
+            </p>
+            <Button>Contact me</Button>
+          </section>
+        </div>
+      </Layout>
+    </>
   );
 }
