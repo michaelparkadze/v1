f193c8c685ee22e4ba069a2b48fe9b0d9c523557
Michael Parkadze <52443367+michaelparkadze@users.noreply.github.com>
2021-04-23 21:28:51 +0300

    [Update] Michael illustration and about me

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Image from "next/image";
 import Layout from "../components/Layout";
 import Button from "../components/Button";
 import styles from "../styles/home.module.scss";
@@ -12,7 +13,7 @@ export default function Home() {
       </Head>
       <Layout>
         <div className={styles.container}>
-          <section>
+          <section className={styles.welcome}>
             <h1>
               Hello I am Michael. <br /> A software engineer.
             </h1>
@@ -21,6 +22,29 @@ export default function Home() {
               porta lorem morbi hendrerit. Maecenas et, at quis purus.
             </p>
             <Button>Contact me</Button>
+            <div className={styles.illustration}>
+              <div className={styles.background}></div>
+              <div className={styles.person}></div>
+              <div className={styles.note1}></div>
+              <div className={styles.note2}></div>
+            </div>
+          </section>
+          <hr />
+          <section className={styles.about}>
+            <h2>About me</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Neque
+              tempor, malesuada adipiscing congue diam. Quis orci amet porttitor
+              blandit amet nullam sit. Elit, purus blandit non ut non quam
+              curabitur.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Neque
+              tempor, malesuada adipiscing congue diam. Quis orci amet porttitor
+              blandit amet nullam sit. Elit, purus blandit non ut non quam
+              curabitur.
+            </p>
+            <Button>More about me</Button>
           </section>
         </div>
       </Layout>
