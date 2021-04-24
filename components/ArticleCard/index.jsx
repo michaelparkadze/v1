2d9c73652c5964376967cb744336aad15cb29968
Michael Parkadze <52443367+michaelparkadze@users.noreply.github.com>
2021-04-24 14:31:20 +0300

    [Update] Article card component

---
+++ components/ArticleCard/index.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { format, parseISO } from "date-fns";
+import styles from "./styles.module.scss";
+
+export default function ArticleCard(props) {
+  const { title, description, topic, date, slug, readTime } = props;
+  return (
+    <Link href={slug}>
+      <div className={styles.card}>
+        <div className={styles.title}>{title}</div>
+        <div className={styles.description}>{description}</div>
+        <div className={styles.extra}>
+          <div className={styles.date}>
+            <span> {format(parseISO(date), "MMM d, uuu")}</span>
+            <span className={styles.divider}></span>
+            <span>{topic}</span>
+          </div>
+
+          <div className={styles.readtime}>
+            <Image src="/assets/icons/book.svg" height={18} width={18} />
+            <span>{readTime} min read</span>
+          </div>
+        </div>
+      </div>
+    </Link>
+  );
+}
