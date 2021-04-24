75c5c2805bda879e0d13c929a40db9a39bacf703
Michael Parkadze <52443367+michaelparkadze@users.noreply.github.com>
2021-04-24 14:58:30 +0300

    [Update] Scroll to top component

--- components/Layout/index.jsx
+++ components/Layout/index.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "../Header";
+import ScrollToTop from "../ScrollToTop";
 
 export default function Layout({ children }) {
   return (
     <>
       <Header />
       <main>{children}</main>
+      <ScrollToTop />
     </>
   );
 }
---
+++ components/ScrollToTop/index.jsx
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from "react";
+import Image from "next/image";
+import styles from "./styles.module.scss";
+
+export default function ScrollToTop() {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const toggleVisibility = () => {
+    if (window.pageYOffset > 400) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  };
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", toggleVisibility);
+  }, []);
+  return (
+    <div className={styles.scrollToTop}>
+      <button onClick={scrollToTop} style={{ opacity: isVisible ? "1" : "0" }}>
+        <Image src="/assets/icons/up-arrow.svg" height={24} width={24} />
+      </button>
+    </div>
+  );
+}
