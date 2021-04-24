86ad40a1183f65eaeaf282c545f73d10b9042545
Michael Parkadze <52443367+michaelparkadze@users.noreply.github.com>
2021-04-24 20:12:09 +0300

    [Update] Responsive header component desktop

--- components/Header/index.jsx
+++ components/Header/index.jsx
@@ -1,11 +1,14 @@
 import React, { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
+import useWindowDimensions from "../../hooks/useWindowDimensions";
 import Menu from "../Menu";
 import styles from "./styles.module.scss";
 
 export default function Header() {
   const [open, setOpen] = useState(false);
+  const { height, width } = useWindowDimensions();
+  const isMobile = width <= 768;
 
   const toggleMenu = () => {
     setOpen(!open);
@@ -17,11 +20,33 @@ export default function Header() {
         <div className={styles.logo}>
           <Link href="/">MP</Link>
         </div>
-        <div className={styles.toggle}>
-          <button onClick={toggleMenu}>
-            <Image src="/assets/icons/menu.svg" height={28} width={28} />
-          </button>
-        </div>
+        {isMobile ? (
+          <div className={styles.toggle}>
+            <button onClick={toggleMenu}>
+              <Image src="/assets/icons/menu.svg" height={28} width={28} />
+            </button>
+          </div>
+        ) : (
+          <nav className={styles.nav}>
+            <ul>
+              <Link href="#welcome">
+                <li>Home</li>
+              </Link>
+              <Link href="#about">
+                <li>About</li>
+              </Link>
+              <Link href="#projects">
+                <li>Projects</li>
+              </Link>
+              <Link href="#articles">
+                <li>Articles</li>
+              </Link>
+              <Link href="#contact">
+                <li>Contact</li>
+              </Link>
+            </ul>
+          </nav>
+        )}
       </header>
       <Menu open={open} toggleMenu={toggleMenu} />
     </>
