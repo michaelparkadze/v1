54fa99df0f5decd3ae54d595e60c9f6ec005a880
Michael Parkadze <52443367+michaelparkadze@users.noreply.github.com>
2021-04-26 11:26:13 +0300

    [Update] Article load animation

--- components/Article/index.jsx
+++ components/Article/index.jsx
@@ -5,30 +5,65 @@ import ReactMarkdown from "react-markdown/with-html";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import codeStyle from "react-syntax-highlighter/dist/cjs/styles/prism/dracula";
 import styles from "./styles.module.scss";
+import { motion } from "framer-motion";
 
 export default function Article(props) {
   const { title, date, content, previousArticle, nextArticle } = props;
+
+  const variants = {
+    visible: {
+      opacity: 1,
+      y: 0,
+    },
+    hidden: { opacity: 0, y: 72 },
+  };
+
+  const variants2 = {
+    visible: {
+      opacity: 1,
+    },
+    hidden: { opacity: 0 },
+  };
 
   return (
     <div className={styles.container}>
       {/* Article Header */}
       <header className={styles.header}>
-        <h1>
-          {title}
-          <p> {format(parseISO(date), "MMMM do, uuu")}</p>
-        </h1>
+        <motion.div
+          animate="visible"
+          initial="hidden"
+          variants={variants}
+          transition={{
+            duration: 0.8,
+            ease: [0.6, 0.05, -0.01, 0.9],
+            delay: 0.45,
+          }}
+        >
+          <h1>
+            {title}
+            <p> {format(parseISO(date), "MMMM do, uuu")}</p>
+          </h1>
+        </motion.div>
       </header>
       {/* Article Content */}
-      <article>
-        <section>
+      <article className={styles.article}>
+        <motion.section
+          animate="visible"
+          initial="hidden"
+          variants={variants}
+          transition={{
+            duration: 0.8,
+            ease: [0.6, 0.05, -0.01, 0.9],
+            delay: 0.65,
+          }}
+        >
           <ReactMarkdown
             escapeHtml={false}
             source={content}
             renderers={{ code: CodeBlock }}
           />
-        </section>
+        </motion.section>
       </article>
-
       {/* Navigation */}
       <nav className={styles.nav}>
         <h2>Read next</h2>
